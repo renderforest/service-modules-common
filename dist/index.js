48a7c2ac7336f93cf83b68f580ca2653f00b22c9
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-10-17 14:55:42 +0400

    service: add built re-exporting index.js file.

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+/*  */
+'use strict'
+
+module.exports.Logger = require('../dist/services/logger')
+module.exports.ResourceUsage = require('../dist/services/resource-usage')
+module.exports.HttpApi = require('../dist/services/http-api')

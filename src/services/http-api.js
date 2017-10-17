74e869755176b065c820a6ee01c38bb209d433fb
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-10-17 16:43:43 +0400

    service: add authorizedRequest() in HttpApi service
     - gets signKey, clientId and returns function
     - returned function gets options argument
     - used service-auth module to set authorization
     - reused request() to make request.

--- src/services/http-api.js
+++ src/services/http-api.js
@@ -2,6 +2,7 @@
 'use strict'
 
 const Promise = require('bluebird')
+const AuthService = require('service-auth')
 const promiseRequest = require('request-promise')
 
 /**
@@ -13,7 +14,7 @@ const promiseRequest = require('request-promise')
  * @param {boolean} [options.json = true]
  * @returns {Promise}
  * @description Makes request with given options.
- *  In case of connection failure try `retryCount` times after `retryDelay` intervals.
+ *  In case of connection failure tries `retryCount` times after `retryDelay` intervals.
  */
 function request (options: HttpAPIRequestOptions) {
   const _options = Object.assign({}, options)
@@ -38,6 +39,22 @@ function request (options: HttpAPIRequestOptions) {
   return retryRequest(_options, retryCount)
 }
 
+/**
+ * @param {string} signKey
+ * @param {string} clientId
+ * @returns {function(object)} - Returns function which gets `options` object and returns Promise.
+ * @description Makes authorized request with given signKey, clientId, and options.
+ *  In case of connection failure tries `retryCount` times after `retryDelay` intervals.
+ */
+function authorizedRequest (signKey, clientId) {
+  return (options) => {
+    const finalOptions = AuthService.setAuthorization(options, signKey, clientId)
+
+    return request(finalOptions)
+  }
+}
+
 module.exports = {
-  request
+  request,
+  authorizedRequest
 }

4a6267664679b7b92c375c78912bca361df7a0cc
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-10-17 17:00:21 +0400

    test: minor update in single test example.

--- __tests__/services/http-api.js
+++ __tests__/services/http-api.js
@@ -1,5 +1,6 @@
 const lib = require('../../dist/services/http-api')
 
-test('Testing esiminch', () => {
+test('Testing TBD', () => {
   expect(lib.request).toBeDefined()
+  expect(lib.authorizedRequest).toBeDefined()
 })

a94fe88d3a78439c546788f56191a65dcdeb8faa
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-10-30 11:26:14 +0400

    test: remove spread operator from test mock - failed to build v6.

--- __mock__/services/http-api.mock.js
+++ __mock__/services/http-api.mock.js
@@ -10,11 +10,7 @@ const Promise = require('bluebird')
  * @description Assigning signKey and clientId to options object(mocked).
  */
 const AuthServiceMocked = (options, signKey, clientId) => {
-  return {
-    ...options,
-    signKey: signKey,
-    clientId: clientId
-  }
+  return Object.assign({}, options, {signKey, clientId})
 }
 
 /**
@@ -24,10 +20,8 @@ const AuthServiceMocked = (options, signKey, clientId) => {
  * @description If options object have truthy resolve property, then returns Promise resolve, else rejects it.
  */
 const promiseRequestMocked = (options, retries) => {
-  const optionsWithRetries = {
-    ...options,
-    retries: retries
-  }
+  const optionsWithRetries = Object.assign({}, options, {retries})
+
   return options.resolve === true ? Promise.resolve(optionsWithRetries) : Promise.reject(optionsWithRetries)
 }
 

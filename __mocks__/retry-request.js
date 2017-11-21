41736057fe23f3190b9d63637f84c8781c0d3346
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-11-21 19:30:19 +0400

    mocks: fix in retry-request mock for Object assign values.

--- __mocks__/retry-request.js
+++ __mocks__/retry-request.js
@@ -2,7 +2,7 @@
 
 module.exports = (options, retries, delay) => {
   const result = {}
-  Object.assign(result, options, retries, delay)
+  Object.assign(result, options, {retries, delay})
 
   return Promise.resolve(result)
 }

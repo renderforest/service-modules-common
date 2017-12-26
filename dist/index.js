b5c858e4fa5d5a686a1d1389b540d22f90466879
narekhovhannisyan <narekgilmour@gmail.com>
2017-12-26 14:24:33 +0400

    rate-limiter: fix JSDoc for checkAbuseUser()

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,4 @@
 module.exports.Logger = require('../dist/services/logger')
 module.exports.ResourceUsage = require('../dist/services/resource-usage')
 module.exports.HttpApi = require('../dist/services/http-api')
+module.exports.RateLimiter = require('../dist/services/rate-limiter').checkAbuseUser()

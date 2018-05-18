e6793946fba21111c97149e3081c2dbd09387e46
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2018-05-18 12:01:38 +0400

    lint: apply linting.

--- __mocks__/retry-request.js
+++ __mocks__/retry-request.js
@@ -1,8 +1,6 @@
-'use strict'
-
 module.exports = (options, retries, delay) => {
   const result = {}
-  Object.assign(result, options, {retries, delay})
+  Object.assign(result, options, { retries, delay })
 
   return Promise.resolve(result)
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 module.exports.Logger = require('./services/logger')
 module.exports.ResourceUsage = require('./services/resource-usage')
 module.exports.HttpApi = require('./services/http-api')
